8705b932ca6007fe5e73a2591585c4b2c69a3a9a
Малько Егор <emalko@diasoft.ru>
2022-02-08 11:22:52 +0300

    reworked

--- src/ru/diasoft/Main.java
+++ src/ru/diasoft/Main.java
@@ -3,6 +3,7 @@ package ru.diasoft;
 
 import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
@@ -12,9 +13,11 @@ import java.nio.file.Paths;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    Reader r = null;
+    BufferedWriter r = Files.newBufferedWriter(Paths.get("output.txt"));
     try {
-      r = Files.newBufferedReader(Paths.get("aba"));
+      r.write("1");
+      r.write("2");
+      r.write("3");
       throw new Exception();
     } catch (IOException exc) {
       System.out.println("IOException: " + exc.getMessage());

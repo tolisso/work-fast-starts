7b856b21b437ed10ab4167c8b994181ef4d26054
Малько Егор <emalko@diasoft.ru>
2022-03-22 15:51:33 +0300

    microservice module 5

--- src/main/java/com/tolisso/bsmicro/dom/User.java
+++ src/main/java/com/tolisso/bsmicro/dom/User.java
@@ -2,18 +2,18 @@ package com.tolisso.bsmicro.dom;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "appuser")
 public class User {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 }
